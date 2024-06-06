2a1a88fe6174805adf4665cdf4b208c41f47be2f
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-06 15:02:43 +0100

    added final css

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const welcomeMes = document.getElementById("welcomeMes");
   const contentMes = document.getElementById("contentMes");
+  const head = document.getElementById("keycode");
 
   const eventKey = document.getElementById("eventKey");
   const eventWhich = document.getElementById("eventWhich");
   const eventCode = document.getElementById("eventCode");
+  const eventLocation = document.getElementById("eventLocation");
 
   function keycode(event) {
-    const { key, which, code } = event;
-    keycode.textContent = which;
+    const { key, which, code, location } = event;
+    head.textContent = key;
     eventKey.innerHTML = key;
     eventWhich.innerHTML = which;
     eventCode.innerHTML = code;
+    eventLocation.innerHTML = location;
 
     contentMes.classList.remove("hidden");
     welcomeMes.remove();
